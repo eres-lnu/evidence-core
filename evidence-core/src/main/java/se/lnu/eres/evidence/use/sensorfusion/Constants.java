96c6a494e45f58e7d830afb40009f30c669d5eb9
Diego Perez <diego.perez@lnu.se>
2024-06-28 11:43:39 +0200

    Add use of the theory for the sensor fusion use case. First commit

---
+++ evidence-core/src/main/java/se/lnu/eres/evidence/use/sensorfusion/Constants.java
@@ -0,0 +1,34 @@
+package se.lnu.eres.evidence.use.sensorfusion;
+
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Constants {
+
+	public final double alphap;
+	public final double gammab;
+	public final double alphab;
+	public final double gammac;
+	public final double alphac;
+	public final double alphat;
+	public final double cc;
+	public final double alpha;
+	public final double beta;
+
+	public Constants(String filename) throws FileNotFoundException, IOException {
+		Properties p = new Properties();
+		p.load(new FileInputStream(filename));
+		this.alphap=Double.valueOf(p.getProperty("alphap"));
+		this.gammab=Double.valueOf(p.getProperty("gammab"));
+		this.alphab=Double.valueOf(p.getProperty("alphab"));
+		this.gammac=Double.valueOf(p.getProperty("gammac"));
+		this.alphac=Double.valueOf(p.getProperty("alphac"));
+		this.alphat=Double.valueOf(p.getProperty("alphat"));
+		this.cc=Double.valueOf(p.getProperty("cc"));
+		this.alpha=Double.valueOf(p.getProperty("alpha"));
+		this.beta=Double.valueOf(p.getProperty("beta"));
+	}
+	
+}
---
+++ evidence-core/src/main/java/se/lnu/eres/evidence/use/sensorfusion/Fusion.java
@@ -0,0 +1,14 @@
+package se.lnu.eres.evidence.use.sensorfusion;
+
+import se.lnu.eres.evidence.core.Discourse;
+
+public class Fusion {
+
+	Discourse[][] ds = new Discourse[3][];
+	
+	public void createProblem(String propertiesFilename) {
+		
+		
+	}
+	
+}
